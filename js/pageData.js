cf5972e8eb97ae749c63c88c1be4f103d470e644
Stonedreamer <73815478+Stonedreamer@users.noreply.github.com>
2020-11-02 09:03:29 +0200

    Add files via upload

---
+++ js/pageData.js
@@ -0,0 +1,301 @@
+
+var pageOptions = {
+    "openLinksInSameTab": true,
+    "searchEngineURL": "https://google.com/",
+    "displaySeconds": false,
+    "background": {
+        "randomize": false,
+        "minFileName": 20,
+        "maxFileName": 24,
+        "extension": "jpg",
+        "staticBackground": "backgrounds/999.jpg"
+    }
+}
+
+var columnData = [
+    {
+        "ContainerName": "Banks ",
+       
+        "ChildLinks": [
+            {
+                "DisplayName": "Alpha",
+                "Link": "https://secure.alpha.gr/Login/myalphaweb/en",
+                "Image": "images/alpha.png"
+            },
+            {
+                "DisplayName": "Πειραιώς",
+                "Link": "https://www.winbank.gr/sites/idiwtes/el/Pages/default.aspx",
+                "Image": "images/peiraios.png"
+            },
+            {
+                "DisplayName": "Taxis",
+                "Link": "https://login.gsis.gr/mylogin/login.jsp?bmctx=1DB55AB50C08F2B418903DE4EB7466AD47038BC455E39B9EA82B1EB28CE52BC6&contextType=external&username=string&password=secure_string&challenge_url=https%3A%2F%2Flogin.gsis.gr%2Fmylogin%2Flogin.jsp&ssoCookie=disablehttponly&request_id=-2488020410986622834&authn_try_count=0&locale=en_US&resource_url=https%253A%252F%252Fwww1.gsis.gr%252Ftaxisnet%252Fmytaxisnet",
+                "Image": "images/taxis.png"
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            }
+        ]
+    },
+    {
+        "ContainerName": "Bills",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "Vodaphone",
+                "Link": "https://www.vodafone.gr/my-vodafone-hub/#login-snack",
+                "Image": "images/vodaphone.png"
+            },
+            {
+                "DisplayName": "Wind",
+                "Link": "https://www.wind.gr/auth/login/oidcLogin.jsp/?tx=a317eba2-3239-46c8-9b6d-6c5e76b83caa&auth=eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJAITFERTQuMDAxQy4wMDAyLjAwMDEhMDAwMSEwMDAwLjAwMDEhMDAwOCFFNDhCLkUxRjcuMzQwRi45OEM3IiwiYXVkIjoiaHR0cHM6XC9cL3N3YXMud2luZC5nciIsInR4IjoiMGFiZjNjZmRhM2ZjNmE0YWZkZWIzYWE2ODFkZGI4ZDk1ODllNjJkMGE2ZTRmZGQwNDQ0NWU5ZGUzNmRlNTNlZCIsInNlc3Npb24iOiI3OGIwNzRmMi0xZmU2LTQ1NDItOTFjYi01Y2NjZWJmZTg3ZWIiLCJhdXRob3JpdHkiOiJodHRwczpcL1wvc3dhcy53aW5kLmdyIiwiaXNzIjoiaHR0cHM6XC9cL293YXMud2luZC5nciIsImV4cCI6MTYwNDI1OTYxOSwiaWF0IjoxNjA0MjUzNTU5LCJyZWRpcmVjdF91cmwiOiJodHRwczpcL1wvb3dhcy53aW5kLmdyXC9vaWRjXC9wb3N0bG9naW4uaHRtIn0.UZ8zHRSABE-ZuHOCDowpo0esicDWkMHqpNuAq6hNWdzApeRmeLlF6soMwLtQ7LyHVF-pqNor3zovco9vLPkP1UXssjaRMsJdtQdaI-ONIpL9JnMkKMR1Tvkdbf6o79LuP4_WzDdRaUNTxGCaS-pVOMpgYGRgsekpRnmxHdiaR0E&ui_locales=eyJtZXRob2QiOiAicGFyYW0iLCAibG9jYWxlcyI6IFsiZWxfR1IiLCAiZW5fVVMiXSwgImRlZmF1bHQiOiAiZWxfR1IifQ%3D%3D&tai=eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJAITFERTQuMDAxQy4wMDAyLjAwMDEhMDAwMSEwMDAwLjAwMDEhMDAwOCFFNDhCLkUxRjcuMzQwRi45OEM3IiwiYXVkIjoiaHR0cHM6XC9cL3N3YXMud2luZC5nciIsInR4IjoiMGFiZjNjZmRhM2ZjNmE0YWZkZWIzYWE2ODFkZGI4ZDk1ODllNjJkMGE2ZTRmZGQwNDQ0NWU5ZGUzNmRlNTNlZCIsInNlc3Npb24iOiI3OGIwNzRmMi0xZmU2LTQ1NDItOTFjYi01Y2NjZWJmZTg3ZWIiLCJhdXRob3JpdHkiOiJodHRwczpcL1wvc3dhcy53aW5kLmdyIiwiaXNzIjoiaHR0cHM6XC9cL293YXMud2luZC5nciIsImV4cCI6MTYwNDI1OTYxOSwiaWF0IjoxNjA0MjUzNTU5LCJyZWRpcmVjdF91cmwiOiJodHRwczpcL1wvb3dhcy53aW5kLmdyXC9vaWRjXC9wb3N0bG9naW4uaHRtIn0.UZ8zHRSABE-ZuHOCDowpo0esicDWkMHqpNuAq6hNWdzApeRmeLlF6soMwLtQ7LyHVF-pqNor3zovco9vLPkP1UXssjaRMsJdtQdaI-ONIpL9JnMkKMR1Tvkdbf6o79LuP4_WzDdRaUNTxGCaS-pVOMpgYGRgsekpRnmxHdiaR0E",
+                "Image": "images/wind.png"
+            },
+            {
+                "DisplayName": "Protergia",
+                "Link": "https://myprotergia.intelen.com/login",
+                "Image": "images/protergia.png"
+            },
+            {
+                "DisplayName": "Cosmote",
+                "Link": "https://www.cosmote.gr/selfcare/jsp/dashboard.jsp;jsessionid=lLaE92LixW83Ua_4Zfmx3SH9VuZMg2kV6aLrO2bqzPRHgEVNSQ6k!2142345652?_requestid=51944",
+                "Image": "images/cosmote.png"
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            }
+        ]
+    },
+    {
+        "ContainerName": "News",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "TechSpot",
+                "Link": "https://www.techspot.com/",
+                "Image": "images/techspot.png"
+            },
+            {
+                "DisplayName": "How to Geek",
+                "Link": "https://www.howtogeek.com/",
+                "Image": "images/howtogeek.png"
+            },
+            {
+                "DisplayName": "Open Source",
+                "Link": "https://opensource.com/",
+                "Image": "images/opensource.png"
+            },
+        ]
+    },
+{
+        "ContainerName": "Administration",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "Volumio",
+                "Link": "http://192.168.1.14",
+                "Image": "images/insomnia.png"
+            },
+             {
+                "DisplayName": "Access Point",
+                "Link": "http://192.168.1.20",
+                "Image": "images/adslgr.png"
+            },
+            {
+                "DisplayName": "Openmediavault",
+                "Link": "http://192.168.1.10",
+                "Image": "images/cerebrux.png"
+            },
+            {
+                "DisplayName": "Mstream",
+                "Link": "http://192.168.1.10:3000",
+                "Image": "images/supereverything.png"
+            },
+            {
+                "DisplayName": "Router",
+                "Link": "http://192.168.1.1",
+                "Image": "images/drandroid.png"
+            },
+            {
+                "DisplayName": "Transmission",
+                "Link": "http://192.168.1.10:9091",
+                "Image": "images/avsite.png"
+            }
+        ]
+    },
+{
+        "ContainerName": "Greeks",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "Insomnia",
+                "Link": "https://www.insomnia.gr/forums/forum/56-%CF%80%CF%81%CE%BF%CF%83%CF%86%CE%BF%CF%81%CE%AD%CF%82/",
+                "Image": "images/insomnia.png"
+            },
+             {
+                "DisplayName": "ADSLgr",
+                "Link": "https://adsl.gr/forum/forum.php",
+                "Image": "images/adslgr.png"
+            },
+            {
+                "DisplayName": "Cerebrux",
+                "Link": "https://cerebrux.net",
+                "Image": "images/cerebrux.png"
+            },
+            {
+                "DisplayName": "Super Everything",
+                "Link": "https://supereverything.gr",
+                "Image": "images/supereverything.png"
+            },
+            {
+                "DisplayName": "Doctor Android",
+                "Link": "https://doctorandroid.gr",
+                "Image": "images/drandroid.png"
+            },
+            {
+                "DisplayName": "AV Site",
+                "Link": "https://avsite.gr/forum",
+                "Image": "images/avsite.png"
+            }
+        ]
+    },
+   {
+        "ContainerName": "Audio/Video",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "Online Radio Box",
+                "Link": "https://www.onlineradiobox.com/",
+                "Image": "images/onlineradiobox.png"
+            },
+            {
+                "DisplayName": "Kosmos",
+                "Link": "https://www.howtogeek.com/",
+                "Image": "images/kosmos.png"
+            },
+            {
+                "DisplayName": "Spotify",
+                "Link": "https://opensource.com/",
+                "Image": "images/spotify.png"
+            },
+{
+                "DisplayName": "YouTube",
+                "Link": "https://youtube.com/",
+                "Image": "images/youtube.png"
+            },
+                {
+                "DisplayName": "Kasetophono",
+                "Link": "https://kasetophono.com/",
+                "Image": "images/kasetofono.png"
+            }
+        ]
+    },
+ {
+        "ContainerName": "Shopping",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "E-shop",
+                "Link": "https://www.e-shop.gr",
+                "Image": "images/eshop.png"
+            },
+            {
+                "DisplayName": "Aliexpress",
+                "Link": "https://www.aliexpress.com/",
+                "Image": "images/aliexpress.png"
+            },
+            {
+                "DisplayName": "Amazon UK",
+                "Link": "https://amazon.co.uk/",
+                "Image": "images/amazon.png"
+            },
+{
+                "DisplayName": "Bangood",
+                "Link": "https://banggood.com/",
+                "Image": "images/banggood.png"
+            },
+                {
+                "DisplayName": "Ebay",
+                "Link": "https://ebay.com/",
+                "Image": "images/ebay.png"
+            }
+        ]
+    },
+{
+        "ContainerName": "Torrents",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "Greek Team",
+                "Link": "https://www.greek-team.cc/index.php",
+                "Image": "images/greekteamcc.png"
+            },
+            {
+                "DisplayName": "RU Tracker",
+                "Link": "https://rutracker.org/forum/tracker.php",
+                "Image": "images/rutracker.png"
+            },
+            {
+                "DisplayName": "IP Torrents",
+                "Link": "https://iptorrents.com/indexipt.php",
+                "Image": "images/iptorrents.png"
+            },
+{
+                "DisplayName": "Sport Video",
+                "Link": "https://www.sport-video.org.ua/",
+                "Image": "images/sportvideo.png"
+            },
+                {
+                "DisplayName": "720 Pier",
+                "Link": "https://720pier.ru/index.php?sid=3eabc91563deb2f8013e57c41c95a188",
+                "Image": "images/720pier.png"
+            }
+        ]
+    },
+    {
+        "ContainerName": "Sports",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "NBA Replays",
+                "Link": "https://nbahdreplay.com/",
+                "Image": "images/nbareplay.jpg"
+            },
+            {
+                "DisplayName": "Jumpball",
+                "Link": "https://www.jumpball.com.gr/",
+                "Image": "images/jumpball.jpg"
+            },
+            {
+                "DisplayName": "Gazzetta",
+                "Link": "https://gazzetta.gr/",
+                "Image": "images/gazzetta.jpg"
+            },
+            {
+                "DisplayName": "Sport 24",
+                "Link": "https://sport24.gr/",
+                "Image": "images/sport24.jpg"
+            },
+            {
+                "DisplayName": "The Ringer",
+                "Link": "https://www.theringer.com/",
+                "Image": "images/theringer.png"
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            }
+        ]
+    }
+
+];
